dd38b7e3719ac2979cabc85b097199f864764e61
dadong.gxd <dadong.gxd@alibaba-inc.com>
2023-12-12 11:17:43 +0800

    M prepare_sd_models.sh

--- tools/prepare_sd_models.sh
+++ tools/prepare_sd_models.sh
@@ -6,7 +6,7 @@ cd -
 # you can download SD models from [aliyun](https://virutalbuy-public.oss-cn-hangzhou.aliyuncs.com/share/RichDreamer/models_sd.tar.gz)
 # and then put `$download_sd` file to `pretrained_models/huggingface/hub/`.
 mkdir -p pretrained_models/huggingface/hub/
-wget https://virutalbuy-public.oss-cn-hangzhou.aliyuncs.com/share/RichDreamer/models_sd.tar.gz -O pretrained_models/models_sd.tar.gz
-mv pretrained_models/models_sd.tar.gz pretrained_models/huggingface/hub/
+wget https://virutalbuy-public.oss-cn-hangzhou.aliyuncs.com/share/RichDreamer/models_sd_clip.tar.gz -O pretrained_models/models_sd_clip.tar.gz
+mv pretrained_models/models_sd_clip.tar.gz pretrained_models/huggingface/hub/
 cd pretrained_models/huggingface/hub/
-tar -xvf models_sd.tar.gz ./
+tar -xvf models_sd_clip.tar.gz ./
